44d26b275954e36e6bcd1db5451b8b2750834f34
Ilya 'Alaie' Shafeev <ilya.tanc@gmail.com>
2023-04-12 08:33:17 -0700

    Fix resposive

--- src/components/Steps/Finish/index.tsx
+++ src/components/Steps/Finish/index.tsx
@@ -3,17 +3,20 @@ import React from "react";
 import { ReactComponent as IconFinish } from "../../../assets/images/icon-thank-you.svg";
 
 import styles from "../../../styles/components/steps/finish.module.scss";
+import Wrap from "../Wrap";
 
 const Finish = () => (
-  <div id={styles.step}>
-    <IconFinish width={80} height={80} />
-    <h1>Thank you!</h1>
-    <p>
-      Thanks for confirming your subscription! We hope you have fun using our
-      platform. If you ever need support, please feel free to email us at
-      support@loremgaming.com.
-    </p>
-  </div>
+  <Wrap>
+    <div id={styles.content}>
+      <IconFinish width={80} height={80} />
+      <h1>Thank you!</h1>
+      <p>
+        Thanks for confirming your subscription! We hope you have fun using our
+        platform. If you ever need support, please feel free to email us at
+        support@loremgaming.com.
+      </p>
+    </div>
+  </Wrap>
 );
 
 export default Finish;
